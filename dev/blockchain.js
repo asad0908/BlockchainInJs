e17ce03a3b436447330b9cc0159abb4d879a84ce
Asad Memon <amdeveloping0908@gmail.com>
2022-12-28 16:10:15 +0530

    updated prototype functions and done testing of our cryptchain

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,11 +1,16 @@
 // MY OWN BLOCKCHAIN!
 
+const sha256 = require("sha256");
+
 function Blockchain() {
   this.chain = []; //stores all the blocks (chain of blocks)
   this.mempool = []; //stores all unverified transactions
+
+  //genesis block
+  this.createNewBlock(786, "0x000000", "0x000000");
 }
 
-Blockchain.prototype.createNewBlock = (nonce, previousHash, hash) => {
+Blockchain.prototype.createNewBlock = function (nonce, previousHash, hash) {
   //creates a new block
   const newBlock = {
     index: this.chain.length + 1,
@@ -26,12 +31,16 @@ Blockchain.prototype.createNewBlock = (nonce, previousHash, hash) => {
   return newBlock;
 };
 
-Blockchain.prototype.getLastBlock = () => {
+Blockchain.prototype.getLastBlock = function () {
   //returns the last block of the chain
   return this.chain[this.chain.length - 1];
 };
 
-Blockchain.prototype.createNewTransaction = (amount, sender, receiver) => {
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  receiver
+) {
   //create a new transaction
   const newTransaction = {
     amount: amount,
@@ -44,6 +53,33 @@ Blockchain.prototype.createNewTransaction = (amount, sender, receiver) => {
 
   //returns to which block index this pending transaction will be added to
   return this.getLastBlock["index"] + 1;
+};
+
+Blockchain.prototype.hashBlock = function (
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  //takes data from block and hash it with a nonce
+  const string =
+    previousBlockHash + JSON.stringify(currentBlockData) + nonce.toString();
+  const hash = sha256(string);
+  return hash;
+};
+
+Blockchain.prototype.proofOfWork = function (
+  previousBlockHash,
+  currentBlockData
+) {
+  //proof of work method to find the correct nonce
+  let nonce = 0;
+  let currentHash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  while (currentHash.substring(0, 4) !== "0000") {
+    //keep incrementing hash until you find the correct proof of your work.
+    nonce++;
+    currentHash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  }
+  return nonce;
 };
 
 module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,63 @@
+const Blockchain = require("./blockchain");
+
+const CryptCoin = new Blockchain();
+
+CryptCoin.createNewTransaction(100, "Asad", "Ayan");
+CryptCoin.createNewTransaction(100, "Asad", "Shafi");
+CryptCoin.createNewTransaction(100, "Asad", "Kalim");
+
+const nonceGot = CryptCoin.proofOfWork(
+  CryptCoin.getLastBlock().previousHash,
+  CryptCoin.mempool
+);
+
+CryptCoin.createNewBlock(
+  nonceGot,
+  CryptCoin.getLastBlock().hash,
+  CryptCoin.hashBlock(
+    CryptCoin.getLastBlock().hash,
+    CryptCoin.mempool,
+    nonceGot
+  )
+);
+
+CryptCoin.createNewTransaction(300, "Ayan", "Shafi");
+CryptCoin.createNewTransaction(10, "Shafi", "Ayan");
+CryptCoin.createNewTransaction(90, "Ayan", "Asad");
+
+const nonceGot2 = CryptCoin.proofOfWork(
+  CryptCoin.getLastBlock().hash,
+  CryptCoin.mempool
+);
+
+CryptCoin.createNewBlock(
+  nonceGot,
+  CryptCoin.getLastBlock().hash,
+  CryptCoin.hashBlock(
+    CryptCoin.getLastBlock().hash,
+    CryptCoin.mempool,
+    nonceGot2
+  )
+);
+
+CryptCoin.createNewTransaction(5, "Maaz", "Ahmed");
+CryptCoin.createNewTransaction(10, "Ahmed", "Rizwan");
+CryptCoin.createNewTransaction(26, "Rehaan", "Shafi");
+CryptCoin.createNewTransaction(16, "Zeerufi", "Youtuber");
+
+const nonceGot3 = CryptCoin.proofOfWork(
+  CryptCoin.getLastBlock().hash,
+  CryptCoin.mempool
+);
+
+CryptCoin.createNewBlock(
+  nonceGot,
+  CryptCoin.getLastBlock().hash,
+  CryptCoin.hashBlock(
+    CryptCoin.getLastBlock().hash,
+    CryptCoin.mempool,
+    nonceGot3
+  )
+);
+
+console.log(CryptCoin);
