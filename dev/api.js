841bf6be4917395d434d26afdf06efadf4ce5954
Asad Memon <amdeveloping0908@gmail.com>
2022-12-29 11:36:15 +0530

    fixed issues and made express server

--- dev/api.js
+++ dev/api.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+//returns the full blockchain
+app.get("/blockchain", (req, res) => {});
+
+//post a new transaction to the mempool
+app.post("/transaction", (req, res) => {});
+
+//mine a new block in our blockchain
+app.get("/mine", (req, res) => {});
 
 app.listen(5000, () => {
   console.log("app is running on port 5000");
